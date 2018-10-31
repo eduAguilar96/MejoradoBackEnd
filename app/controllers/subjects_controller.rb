098a1623384de69776bf4c6615b4fee9c2ac7479
eduAguilar96 <eduxx77@gmail.com>
2018-10-31 15:51:47 -0600

    Create Controller concerns

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,77 +1,36 @@
 class SubjectsController < ApplicationController
   before_action :set_subject, only: [:show, :edit, :update, :destroy]
 
-  # GET /subjects
-  # GET /subjects.json
   def index
     @subjects = Subject.all
-    render json: @subjects
+    json_response(@subjects)
   end
 
-  # GET /subjects/1
-  # GET /subjects/1.json
-  def show
-    @subject = Subject.find(params[:id])
-    render json: @subject
-  end
-
-  # GET /subjects/new
-  def new
-    @subject = Subject.new
-  end
-
-  # GET /subjects/1/edit
-  def edit
-  end
-
-  # POST /subjects
-  # POST /subjects.json
   def create
-    @subject = Subject.new(create_params)
-
-    respond_to do |format|
-      if @subject.save
-        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
-        format.json { render :show, status: :created, location: @subject }
-      else
-        format.html { render :new }
-        format.json { render json: @subject.errors, status: :unprocessable_entity }
-      end
-    end
+    @subject = Subject.create!(subject_params)
+    json_response(@subject, :created)
   end
 
-  # PATCH/PUT /subjects/1
-  # PATCH/PUT /subjects/1.json
+  def show
+    json_response(@subject)
+  end
+
   def update
-    respond_to do |format|
-      if @subject.update(subject_params)
-        format.html { redirect_to @subject, notice: 'Subject was successfully updated.' }
-        format.json { render :show, status: :ok, location: @subject }
-      else
-        format.html { render :edit }
-        format.json { render json: @subject.errors, status: :unprocessable_entity }
-      end
-    end
+    @subject.update(subject_params)
+    head :no_content
   end
 
-  # DELETE /subjects/1
-  # DELETE /subjects/1.json
   def destroy
     @subject.destroy
-    respond_to do |format|
-      format.html { redirect_to subjects_url, notice: 'Subject was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_subject
       @subject = Subject.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def create_params
+    def subject_params
       params.require(:subject).permit(
         :text
       )
