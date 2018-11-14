3b9ae64e5f123b6557520403a605efc80e553687
eduAguilar96 <eduxx77@gmail.com>
2018-11-13 23:33:20 -0600

    Fix routes.rb DELETE bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :answers, only: [:index, :show, :create, :update, :destoy]
-  resources :exams, only: [:index, :show, :create, :update, :destoy]
-  resources :questions, only: [:index, :show, :create, :update, :destoy]
-  resources :subjects, only: [:index, :show, :create, :update, :destoy]
-  resources :text_fields, only: [:index, :show, :create, :update, :destoy]
-  resources :variables, only: [:index, :show, :create, :update, :destoy]
+  resources :answers, only: [:index, :show, :create, :update, :destroy]
+  resources :exams, only: [:index, :show, :create, :update, :destroy]
+  resources :questions, only: [:index, :show, :create, :update, :destroy]
+  resources :subjects, only: [:index, :show, :create, :update, :destroy]
+  resources :text_fields, only: [:index, :show, :create, :update, :destroy]
+  resources :variables, only: [:index, :show, :create, :update, :destroy]
 
   get '/exams/:id/gen/:times', to: 'exams#generate'
 end
